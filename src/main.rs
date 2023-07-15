a18cf827d606c52f414f921da831fe221b59bece
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-07-15 21:43:31 +0200

    Show image

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,58 @@
 use bevy::prelude::*;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        Camera3dBundle::default(),
-    ));
+    commands.spawn((Camera3dBundle::default(),));
     // root node
-    commands.spawn(NodeBundle {
-        style: Style {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            justify_content: JustifyContent::SpaceBetween,
-            ..default()
-        },
-        ..default()
-    }).with_children(|parent| {
-        parent.spawn((TextBundle::from_section(
-            "Text Example",
-            TextStyle {
-                font: asset_server.load("kenney_ui-pack/Font/kenvector_future.ttf"),
-                font_size: 30.0,
-                color: Color::WHITE,
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
             },
-        )));
-    });
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                "Text Example",
+                TextStyle {
+                    font: asset_server.load("kenney_ui-pack/Font/kenvector_future.ttf"),
+                    font_size: 30.0,
+                    color: Color::WHITE,
+                },
+            ));
+            parent
+                .spawn((
+                    NodeBundle {
+                        style: Style {
+                            width: Val::Px(500.0),
+                            height: Val::Px(125.0),
+                            margin: UiRect::top(Val::VMin(5.)),
+                            ..default()
+                        },
+                        // a `NodeBundle` is transparent by default, so to see the image we have to its color to `WHITE`
+                        background_color: Color::WHITE.into(),
+                        ..default()
+                    },
+                    UiImage::new(asset_server.load("kenney_ui-pack/PNG/green_button00.png")),
+                ))
+                .with_children(|parent| {
+                    // alt text
+                    // This UI node takes up no space in the layout and the `Text` component is used by the accessiblity module
+                    // and is not rendered.
+                    parent.spawn((
+                        NodeBundle {
+                            style: Style {
+                                display: Display::None,
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        },
+                        Text::from_section("Bevy logo", TextStyle::default()),
+                    ));
+                });
+        });
 }
 
 fn main() {
