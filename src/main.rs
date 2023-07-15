c54fb988e7ddc1c8bd861e956489f682d7e5736b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-07-15 21:49:09 +0200

    Really bad button

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,75 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             style: Style {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
-                justify_content: JustifyContent::SpaceBetween,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
                 ..default()
             },
             ..default()
         })
         .with_children(|parent| {
-            parent.spawn(TextBundle::from_section(
-                "Text Example",
-                TextStyle {
-                    font: asset_server.load("kenney_ui-pack/Font/kenvector_future.ttf"),
-                    font_size: 30.0,
-                    color: Color::WHITE,
-                },
-            ));
             parent
-                .spawn((
-                    NodeBundle {
-                        style: Style {
-                            width: Val::Px(500.0),
-                            height: Val::Px(125.0),
-                            margin: UiRect::top(Val::VMin(5.)),
-                            ..default()
-                        },
-                        // a `NodeBundle` is transparent by default, so to see the image we have to its color to `WHITE`
-                        background_color: Color::WHITE.into(),
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
                         ..default()
                     },
-                    UiImage::new(asset_server.load("kenney_ui-pack/PNG/green_button00.png")),
-                ))
+                    border_color: BorderColor(Color::BLACK),
+                    background_color: Color::rgb(0.15, 0.15, 0.15).into(),
+                    ..default()
+                })
                 .with_children(|parent| {
-                    // alt text
-                    // This UI node takes up no space in the layout and the `Text` component is used by the accessiblity module
-                    // and is not rendered.
-                    parent.spawn((
-                        NodeBundle {
-                            style: Style {
-                                display: Display::None,
-                                ..Default::default()
+                    parent
+                        .spawn((
+                            NodeBundle {
+                                style: Style {
+                                    width: Val::Px(500.0),
+                                    height: Val::Px(125.0),
+                                    margin: UiRect::top(Val::VMin(5.)),
+                                    // horizontally center child text
+                                    justify_content: JustifyContent::Center,
+                                    // vertically center child text
+                                    align_items: AlignItems::Center,
+                                    ..default()
+                                },
+                                // a `NodeBundle` is transparent by default, so to see the image we have to its color to `WHITE`
+                                background_color: Color::WHITE.into(),
+                                ..default()
                             },
-                            ..Default::default()
-                        },
-                        Text::from_section("Bevy logo", TextStyle::default()),
-                    ));
+                            UiImage::new(
+                                asset_server.load("kenney_ui-pack/PNG/green_button00.png"),
+                            ),
+                        ))
+                        .with_children(|parent| {
+                            // alt text
+                            // This UI node takes up no space in the layout and the `Text` component is used by the accessiblity module
+                            // and is not rendered.
+                            parent.spawn((
+                                NodeBundle {
+                                    style: Style {
+                                        display: Display::None,
+                                        ..Default::default()
+                                    },
+                                    ..Default::default()
+                                },
+                                Text::from_section("Bevy logo", TextStyle::default()),
+                            ));
+                            parent.spawn(TextBundle::from_section(
+                                "Text Example",
+                                TextStyle {
+                                    font: asset_server
+                                        .load("kenney_ui-pack/Font/kenvector_future.ttf"),
+                                    font_size: 30.0,
+                                    color: Color::WHITE,
+                                },
+                            ));
+                        });
                 });
         });
 }
